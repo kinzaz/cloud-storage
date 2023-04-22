bd5f64b999e78287704761cdc18458f15780bf48
Vladislav Vlasenko <kinzazdev579@gmail.com>
2023-04-22 17:36:31 +0300

    configuration env, multer, serve-static

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,18 +6,25 @@ import { FilesModule } from './files/files.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from './users/entities/user.entity';
 import { FileEntity } from './files/entities/file.entity';
+import { ConfigModule } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'snuffleupagus.db.elephantsql.com',
-      port: 5432,
-      username: 'nfxutejw',
-      password: 'Z_3f7muZgbwEOZ8SLOSRCfz2cFrimNcK',
-      database: 'nfxutejw',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT) || 5432,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       entities: [UserEntity, FileEntity],
       synchronize: true,
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'uploads'),
     }),
     UsersModule,
     FilesModule,
